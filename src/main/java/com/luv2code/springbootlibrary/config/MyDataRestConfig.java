5d4938ee2d189ac33080b58c3c3b382f1c58df1d
Rob Trostle <rtrostle@nipr.com>
2024-06-19 09:50:35 -0400

    Adding Review repository

--- src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
@@ -5,6 +5,7 @@ package com.luv2code.springbootlibrary.config;
 //import com.luv2code.springbootlibrary.entity.Review;
 
 import com.luv2code.springbootlibrary.entity.Book;
+import com.luv2code.springbootlibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -26,11 +27,11 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         HttpMethod.PUT};
 
     config.exposeIdsFor(Book.class);
-//    config.exposeIdsFor(Review.class);
+    config.exposeIdsFor(Review.class);
 //    config.exposeIdsFor(Message.class);
 //
-//    disableHttpMethods(Book.class, config, theUnsupportedActions);
-//    disableHttpMethods(Review.class, config, theUnsupportedActions);
+    disableHttpMethods(Book.class, config, theUnsupportedActions);
+    disableHttpMethods(Review.class, config, theUnsupportedActions);
 //    disableHttpMethods(Message.class, config, theUnsupportedActions);
 
     /* Configure CORS Mapping */
