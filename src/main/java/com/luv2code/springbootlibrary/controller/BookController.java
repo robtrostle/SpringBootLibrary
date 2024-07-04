83a0f2d0163daa76c5a7f2339c6d95afffd7b4a9
Rob Trostle <rtrostle@nipr.com>
2024-07-04 16:30:54 -0400

    implement review functionality & security

--- src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
+++ src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
@@ -2,10 +2,12 @@ package com.luv2code.springbootlibrary.controller;
 
 import com.luv2code.springbootlibrary.entity.Book;
 import com.luv2code.springbootlibrary.service.BookService;
+import com.luv2code.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,20 +25,23 @@ public class BookController {
   }
 
   @GetMapping("/secure/currentloans/count")
-  public int currentLoansCount(){
-    String userEmail = "testuser@email.com";
+  public int currentLoansCount(@RequestHeader("Authorization") String token){
+    String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
     return bookService.currentLoansCount(userEmail);
   }
 
   @GetMapping("/secure/ischeckedout/byuser")
-  public Boolean checkoutBookByUser(@RequestParam Long bookId){
-    String userEmail = "testuser@email.com";
+  public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token,
+      @RequestParam Long bookId) {
+    String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
     return bookService.checkoutBookByUser(userEmail, bookId);
   }
 
   @PutMapping("/secure/checkout")
-  public Book checkoutBook(@RequestParam Long bookId) throws Exception {
-    String userEmail = "testuser@email.com";
+  public Book checkoutBook(@RequestParam Long bookId,
+                            @RequestHeader("Authorization") String token) throws Exception {
+    String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+
     return bookService.checkoutBook(userEmail, bookId);
   }
 
