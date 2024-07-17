bd7984934287138236fcb93b4db196d487bc8f97
Robert Trostle <51966615+robtrostle@users.noreply.github.com>
2024-07-17 09:38:31 -0400

    updating urls for connection to git pages

--- src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
+++ src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://robtrostle.github.io/react-library")
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
--- src/main/java/com/luv2code/springbootlibrary/controller/MessagesController.java
+++ src/main/java/com/luv2code/springbootlibrary/controller/MessagesController.java
@@ -8,7 +8,7 @@ import com.luv2code.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://robtrostle.github.io/react-library")
 @RestController
 @RequestMapping("/api/messages")
 public class MessagesController {
